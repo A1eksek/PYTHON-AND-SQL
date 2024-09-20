567dfd0bbae6c1974f4cb8be2cedcafb1c8f3dbd
A1eksek <Alexey20080705@gmail.com>
2024-09-20 14:06:45 +0400

    Update sqlpyt.py

--- sqlpyt.py
+++ sqlpyt.py
@@ -1,47 +1,55 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname='testdb1', user='postgres', password='1111', host='127.0.0.1', port='5432')
+print('0.Создать бд\n1. Добавить клиента \n2. Добавить номер телефона для клиента \n3. Изменение данных клиента \n4. Удалить номер телефона у клиента \n5. Удалить клиента \n6. Найти клиента по данным')
+clien_search = input('Введите номер для команды: ')
+conn = psycopg2.connect(dbname='postgres', user='postgres', password='1111', host='127.0.0.1', port='5432')
 cursor = conn.cursor()
 
-# cursor.execute('CREATE TABLE people (id SERIAL PRIMARY KEY, name VARCHAR(50), age INTEGER)')
+if clien_search == '0':
+    cursor.execute('CREATE TABLE telephone_book (id SERIAL PRIMARY KEY, name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(100), telephone VARCHAR(12))')
+    conn.commit()  # подтверждение транкзации
+    print('ТАБЛИЦА СОЗДАНА')
+
+if clien_search == '1':
+    name = input('Введите имя: ')
+    last_name = input('Введите фамилию: ')
+    email = input('Введите почту: ')
+    telephone = input('Введите телефон (Если не хотите вписывать, номер напишите 0) : ')
+    if telephone == '0':
+        telephone = None
+    people = (name, last_name, email, telephone)
+    cursor.execute("INSERT INTO telephone_book (name, last_name, email, telephone) VALUES(%s, %s, %s, %s)", people)
+
+elif clien_search == '2':
+    telephones = input('Введите номер телефона который хотите добавить')
+    id_cl = input('Введите id клиета для которого добавляется телефон')
+    telep = (telephones, id_cl)
+    cursor.execute("UPDATE telephone_book SET telephone = %s WHERE id=%s", telep)
+
+
+# cursor.execute('CREATE TABLE telephone_book (id SERIAL PRIMARY KEY, name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(100), telephone VARCHAR(12))')
 # conn.commit() #подтверждение транкзации
 # print('ТАБЛИЦА СОЗДАНА')
-# conn.autocommit = True #используется для создания БД
-# sql = 'CREATE DATABASE testdb1'
-# cursor.execute(sql)
-# print('База данных создана')
-# bob = ('Алиса', 68)
-# cursor.execute("INSERT INTO people(name, age) VALUES (%s, %s)", bob)
-# people = [('Костя', 32), ('Маша', 12), ('Алексей', 16)]
-# cursor.executemany("INSERT INTO people(name, age) VALUES(%s, %s)", people) #позваляет вставить набор строк
-# conn.commit()
 
-# cursor.execute("SELECT * FROM people")
-# cursor.execute("SELECT * FROM people WHERE age > 30")
+# people = ('Иван', 'Никитин', 'IvanN2@mail.ru', None)
+# cursor.execute("INSERT INTO telephone_book (name, last_name, email, telephone) VALUES(%s, %s, %s, %s)", people)
+
+# telep = ('89378759075', 2)
+# cursor.execute("UPDATE telephone_book SET telephone = %s WHERE id=%s", telep)
+
+# telep = (None, 2)
+# cursor.execute("UPDATE telephone_book SET telephone = %s WHERE id=%s", telep)
+
+# id_delete = (1,)
+# cursor.execute("DELETE FROM telephone_book WHERE id=%s", id_delete)
+
+# client_id = [('Никитин')]
+# cursor.execute("SELECT * FROM telephone_book WHERE last_name=%s", client_id)
 # print(cursor.fetchall())
 
-# cursor.execute("SELECT * FROM people")
-# for person in cursor.fetchall():
-#     print(f"{person[1]} - {person[2]}")
 
-# cursor.execute('SELECT * FROM people')
-# print(cursor.fetchmany(3))
-# print(cursor.fetchmany(2))
-
-# cursor.execute('SELECT * FROM people')
-# print(cursor.fetchone())
-
-cursor.execute('SELECT * FROM people WHERE id = 2')
-name, age = cursor.fetchone()
-print(f'Name: {name} Age: {age}')
+conn.commit()
 
 
 cursor.close()
 conn.close()
-
-"""
-Метод cursor() объекта connection возращает cursor - объект cursor, через который можно отправлять запросы к бд.
-Для этого класс cursor представляет ряд методов
-
-1) execute(query, vars=None) - выполняет одну SQL инструкцию. Через второй параметр
-"""
